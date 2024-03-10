794ff367b2cfe22f01ebc82daeb26dded386130d
Arslan jafar <arslanbinjaffar12000@gmail.com>
2024-03-10 17:21:19 +0500

    something in code

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,6 +1,6 @@
 
-import SignUpForm from "./auth/Register";
-import SignInForm from "./auth/Login";
+import SignUpForm from "./auth/_Register";
+import SignInForm from "./auth/_Login";
 import Todo from "./todo/Todo";
 
 export { SignInForm, SignUpForm, Todo };
